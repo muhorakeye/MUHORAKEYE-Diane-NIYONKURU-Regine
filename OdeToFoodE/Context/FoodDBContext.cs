da41c6cc98992992cf76b361bab7989efdb65c61
=diane <=muhorakeyed5@gmail.com>
2020-01-28 12:36:40 -0800

    first commit

---
+++ OdeToFoodE/Context/FoodDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFoodE.Models;
+
+namespace OdeToFoodE.Context
+{
+    public class FoodDbContext : DbContext
+    {
+
+        public DbSet<Food> Foods { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=FoodServer.db");
+        }
+    }
+}
---
+++ OdeToFoodE/Models/Restaurent.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFoodE.Models
+{
+    public enum CuisineType
+    {
+        None,
+        African,
+        Italian,
+        Indian,
+        Mexican
+        
+        
+    }
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        
+        [Required, MaxLength(80)]
+        [Display(Name = "Restaurant Name")]
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; } 
+    }
+}
